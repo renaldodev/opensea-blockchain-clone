542435a00ecb27c7b8c0a370fb45057a70215b29
Renaldo Mateus <renaldomateus77.dev@gmail.com>
2022-02-02 23:50:19 +0100

    feat(Hero): Hero add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { Head } from "../components/Head";
+import { Hero } from "../components/Hero";
 
 export default function Home(){
 	return (
 		<div>
 			<Head/>
+			<Hero/>
 		</div>
 	)
 }
